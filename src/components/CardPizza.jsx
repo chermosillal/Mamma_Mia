c1d9c03592e5dc75ad431a4278ddaba55f3051ea
CesarH <cesar.hermosilla@gmail.com>
2025-05-31 16:17:44 -0400

    se generan las card de los productos y sus cess para darlkes formato

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -1,15 +1,20 @@
+import "../assets/css/card.css";
+
 const CardPizza = (props) => {
   return (
-    <div >
-        <img src={props.img} alt={props.name} />
-        <h2 class="nombre">{props.name}</h2>
-        <hr />
-        <p class="titulo_ingredientes">Ingredientes:</p>
-        <p class="titulo_ingredientes">{props.ingredients}</p>
-        <hr />
-        <p class="precio">{props.price}</p>
-        <hr />
-    </div>
+      <div class="card">
+          <img class="foto" src={props.img} alt={props.name} />
+          <div class="nombre"><h2 >Pizza {props.name}</h2></div>
+          <div class="linea"></div>
+          <p class="tituloIngredientes">Ingredientes:</p>
+          <p class="ingredientes"> <i class="fas fa-pizza-slice"></i> {props.ingredients}</p>
+          <div class="linea"></div>
+          <p class="precio">Precio: $ {props.price}</p>
+          <div className="botones">
+            <button class="btnVermas"><i class="fas fa-eye"></i> Ver más</button>
+            <button class="btnAdd"><i class="fas fa-shopping-cart"></i> Añadir</button>
+          </div>
+      </div>
   )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,23 @@
 import CardPizza from "./CardPizza"
 import Header from "./Header"
+import "../assets/css/home.css";
 
 const Home = () => {
   return (
-  <>
-    <Header/>
-    {/* <CardPizza img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9080-784dcc87ec2c"/> */}
-    <CardPizza img="src/assets/img/napolitana.jpg" name="lala" ingredients="lalalaala" price="222"/>
+      <>
+      <Header/>
+      <div class="productos">
 
-    {/* <CardPizza img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fcheese-164872_640_com.jpg?alt=media&token=18b2b821-4d0d-43f2-a1c6-8c57bc388fab"/> */}
-    <CardPizza img="src/assets/img/espanola.jpg" name="lala" ingredients="lalalaala" price="222"/>
+        {/* <CardPizza img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9080-784dcc87ec2c"/> */}
+        <CardPizza img="src/assets/img/napolitana.jpg" name="Napolitana" ingredients={["mozzarella, ", "tomates, ", "jamón, ", "orégano"]} price="5.950"/>
 
-    {/* <CardPizza img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_com.jpg?alt=media&token=e7cde87a-08d5-4040-ac54-90f6c31eb3e3"/> */}
-    <CardPizza img="src/assets/img/peperoni.jpg" name="lala" ingredients="lalalaala" price="222"/>
-  </>
+        {/* <CardPizza img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fcheese-164872_640_com.jpg?alt=media&token=18b2b821-4d0d-43f2-a1c6-8c57bc388fab"/> */}
+        <CardPizza img="src/assets/img/espanola.jpg" name="Española" ingredients={["mozzarella ,", "gorgonzola ,", "parmesano ,", "provolone"]} price="6.950"/>
+
+        {/* <CardPizza img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_com.jpg?alt=media&token=e7cde87a-08d5-4040-ac54-90f6c31eb3e3"/> */}
+        <CardPizza img="src/assets/img/peperoni.jpg" name="Pepperoni" ingredients={["mozzarella ,", "pepperoni ,", "orégano"]} price="6.950"/>
+      </div>
+      </>
   )
 }
 
