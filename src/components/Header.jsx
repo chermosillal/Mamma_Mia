34b7775a2055b4fd932382ad1dce21b3186fbaf2
CesarH <cesar.hermosilla@gmail.com>
2025-05-30 20:31:09 -0400

    Se crea esxtructura base de los componentes y se desarrolla Header.jsx.... se coloca Style.ccs propio y un tipo de letra propio

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div class="cabecera">
+      <h1>!Pizzería Mamma Mia!</h1>
+      <p class="refran">¡Tenemos las mejores pizzas que podrás encontrar!</p>
+    </div>
+  )
+}
+
+export default Header
