d93a6e267674144025e75310ab2bbaa50474bd7a
CesarH <cesar.hermosilla@gmail.com>
2025-06-01 13:44:18 -0400

    mejoras en rotulo del pie de pagina

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <p>© 2025 - Pizzeria Mamma Mia - Todos los derechos reservados</p>
-      <p>Diseño Front-end: Cesar Hermosilla L </p>
+      <p>Desarrollo Front-end: Cesar Hermosilla L - Desafiolatam Chile</p>
     </footer>
   );
 };
