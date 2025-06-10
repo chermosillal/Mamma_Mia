3e7d920ba997a6b783695909dcc6b9b323d4fd03
CesarH <cesar.hermosilla@gmail.com>
2025-06-09 20:07:54 -0400

    manejo de rutas de img

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,13 +9,13 @@ const Home = () => {
       <div class="productos">
 
         {/* <CardPizza img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9080-784dcc87ec2c"/> */}
-        <CardPizza img="./public/napolitana.jpg" name="Napolitana" ingredients={["mozzarella", "tomates", "jamón", "orégano"]} price="5950"/>
+        <CardPizza img="napolitana.jpg" name="Napolitana" ingredients={["mozzarella", "tomates", "jamón", "orégano"]} price="5950"/>
 
         {/* <CardPizza img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fcheese-164872_640_com.jpg?alt=media&token=18b2b821-4d0d-43f2-a1c6-8c57bc388fab"/> */}
-        <CardPizza img="./public/espanola.jpg" name="Española" ingredients={["mozzarella", "gorgonzola", "parmesano", "provolone"]} price="6950"/>
+        <CardPizza img="espanola.jpg" name="Española" ingredients={["mozzarella", "gorgonzola", "parmesano", "provolone"]} price="6950"/>
 
         {/* <CardPizza img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_com.jpg?alt=media&token=e7cde87a-08d5-4040-ac54-90f6c31eb3e3"/> */}
-        <CardPizza img="./public/peperoni.jpg" name="Pepperoni" ingredients={["mozzarella", "pepperoni", "orégano"]} price="6950"/>
+        <CardPizza img="peperoni.jpg" name="Pepperoni" ingredients={["mozzarella", "pepperoni", "orégano"]} price="6950"/>
       </div>
       </>
   )
